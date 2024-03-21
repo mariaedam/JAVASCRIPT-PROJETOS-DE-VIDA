4cbc781ec4677f9cebf1ad1bf9361db7b08ea6f4
mariaedam <111357561+mariaedam@users.noreply.github.com>
2024-03-21 10:03:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick= function(){
+
     botoes[i].onclick = function(){
         
         for(let j=0;j<botoes.length;j++){
@@ -9,4 +11,7 @@ for(let i=0;i <botoes.length;i++){
         }
         botoes[i].classList.add("ativo");
     }
-}
+    botoes[i].classList.add("ativo");
+    }
+    console.log();
+  }
