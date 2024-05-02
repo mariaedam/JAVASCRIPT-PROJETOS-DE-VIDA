9f4ad14edcf9f15f9bc6838eb72f7843a67d55eb
mariaedam <111357561+mariaedam@users.noreply.github.com>
2024-05-02 09:30:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
